b6bff161099327ba9b21274c301119415ef6b72e
Alexandre BARAT <alex.barat@gmail.com>
2020-05-28 19:10:51 +0200

    First admin tests

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  subject {
+    described_class.new(password: 'azerty',
+                        email: 'john@doe.com')
+  }
+  let(:user) { User.new(firstname: 'Alex', lastname: 'Bart', email: 'alex.bart@gmail.com', password: 'azerty') }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a firstname' do
+      subject.firstname = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a lastname' do
+      subject.lastname = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'Capitalization' do
+    let(:john) { Admin.new(firstname: 'jOhN', lastname: 'doE', email: 'john@doe.com', password: 'azerty') }
+    it 'should capitalize the firstname' do
+      expect(john.firstname).to eq('John')
+    end
+  end
+
+  describe 'Managing' do
+    let(:event) { Event.new(title: 'Climate Summit 2020', date: 'Tue, 9 June 2020', description: 'a coordinated national response is more important than ever to protect from the escalating impacts of climate change.', location: 'Online Event', owner: subject, full: false) }
+
+    it 'should change the password' do
+      expect(subject.change(user, 'password', 'querty')).to_not eq('azerty')
+    end
+
+    it 'should change the title' do
+      expect(subject.change(event, 'title', 'Act for Climate 2020')).to_not eq('Climate Summit 2020')
+    end
+  end
+
+  describe 'Option' do
+    it 'should define the firstname as optional' do
+      expect(subject.option(user, ...))
+    end
+  end
+end
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  let(:john) { Admin.new(firstname: 'jOhN', lastname: 'doE', email: 'john@doe.com', password: 'azerty') }
+  subject {
+    described_class.new(title: 'Climate Summit 2020',
+                        date: 'Tue, 9 June 2020',
+                        description: 'a coordinated national response is more important than ever to protect from the escalating impacts of climate change.',
+                        location: 'Online Event',
+                        owner: john,
+                        full: false)
+  }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an date' do
+      subject.date = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a location' do
+      subject.location = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a owner' do
+      subject.owner = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a full' do
+      subject.full = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
